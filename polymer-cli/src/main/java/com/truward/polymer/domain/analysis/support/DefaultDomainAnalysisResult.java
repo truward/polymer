2be0fed2fc0562e79edd23c827bfc4cd4dbc129c
Alexander Shabanov <avshabanov@gmail.com>
2013-11-06 00:10:31 -0800

    Cosmetic renamings, implementer now emits implements

--- polymer-cli/src/main/java/com/truward/polymer/domain/analysis/support/DefaultDomainAnalysisResult.java
+++ polymer-cli/src/main/java/com/truward/polymer/domain/analysis/support/DefaultDomainAnalysisResult.java
@@ -6,9 +6,7 @@ import com.truward.polymer.domain.analysis.DomainAnalysisResult;
 import com.truward.polymer.domain.analysis.DomainField;
 
 import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 import java.lang.reflect.Method;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -44,78 +42,6 @@ public final class DefaultDomainAnalysisResult implements DomainAnalysisResult {
   @Nonnull
   public Collection<? extends DomainField> getDeclaredFields() {
     return declaredFields;
-  }
-
-  /**
-   * Represents method-based field information.
-   */
-  public static final class MethodBasedDomainField implements DomainField {
-    private final String name;
-    private final Method originMethod;
-    private final String getterName;
-    private Boolean nullable;
-
-    private MethodBasedDomainField(@Nonnull String name, @Nonnull Method originMethod) {
-      this.name = name;
-      this.originMethod = originMethod;
-      this.getterName = NamingUtil.createGetterName(getFieldType(), getFieldName());
-
-      // all primitive types are not nullable
-      if (getFieldType() instanceof Class && ((Class) getFieldType()).isPrimitive()) {
-        setNullable(false);
-      }
-    }
-
-    @Override
-    @Nonnull
-    public String getFieldName() {
-      return name;
-    }
-
-    @Override
-    @Nonnull
-    public String getGetterName() {
-      return getterName;
-    }
-
-    @Override
-    @Nonnull
-    public Type getFieldType() {
-      return originMethod.getGenericReturnType();
-    }
-
-    @Nullable
-    @Override
-    public Class<?> getFieldTypeAsClass() {
-      final Type fieldType = getFieldType();
-      return fieldType instanceof Class ? ((Class) fieldType) : null;
-    }
-
-    @Override
-    public boolean isNullable() {
-      if (this.nullable == null) {
-        throw new IllegalStateException("Nullable value is not known");
-      }
-      return nullable;
-    }
-
-    @Override
-    public boolean isNullableUndecided() {
-      return nullable == null;
-    }
-
-    @Override
-    public void setNullable(boolean nullable) {
-      if (this.nullable != null) {
-        throw new IllegalStateException("Unable to assign nullable value twice");
-      }
-      this.nullable = nullable;
-    }
-
-    @Override
-    public String toString() {
-      return name + " : " + getFieldType();
-    }
   }
 
   //
