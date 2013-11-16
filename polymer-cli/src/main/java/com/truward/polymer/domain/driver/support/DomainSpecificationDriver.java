0ae0e7f3cd499f65cb618a14f9ce876887e58cd4
Alexander Shabanov <avshabanov@gmail.com>
2013-11-15 19:23:46 -0800

    Splitting specifier from driver

---
+++ polymer-cli/src/main/java/com/truward/polymer/domain/driver/support/DomainSpecificationDriver.java
@@ -0,0 +1,45 @@
+package com.truward.polymer.domain.driver.support;
+
+import com.google.common.collect.ImmutableList;
+import com.truward.polymer.core.driver.SpecificationDriver;
+import com.truward.polymer.core.driver.SpecificationState;
+import com.truward.polymer.domain.DomainObjectSpecifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+
+/**
+ * Implementation of the specification driver for domain object generator plugin
+ *
+ * @author Alexander Shabanov
+ */
+public final class DomainSpecificationDriver implements SpecificationDriver {
+  private final Logger log = LoggerFactory.getLogger(DomainSpecificationDriver.class);
+  private final DomainObjectSpecifier specifier;
+
+  public DomainSpecificationDriver() {
+    specifier = new DefaultDomainObjectSpecifier();
+  }
+
+  @Nonnull
+  @Override
+  public List<Class<?>> getProvidedResourceClasses() {
+    return ImmutableList.<Class<?>>of(DomainObjectSpecifier.class);
+  }
+
+  @Nonnull
+  @Override
+  public Object provide(@Nonnull Class<?> clazz) {
+    if (clazz.equals(DomainObjectSpecifier.class)) {
+      return specifier;
+    }
+    throw new IllegalStateException("Unable to provide resource for class " + clazz);
+  }
+
+  @Override
+  public void setState(@Nonnull SpecificationState state) {
+    log.debug("Changed state to {}", state);
+  }
+}
