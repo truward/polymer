d8a88af854d5674ce90488a8ff56cdbdc666d888
Alexander Shabanov <avshabanov@gmail.com>
2013-11-25 22:51:25 -0800

    Deleting spec driver: DI will serve as main assembly point

--- polymer-cli/src/main/java/com/truward/polymer/core/util/DefaultValues.java
+++ polymer-cli/src/main/java/com/truward/polymer/core/util/DefaultValues.java
@@ -1,6 +1,8 @@
 package com.truward.polymer.core.util;
 
 /**
+ * Utility class that is aware of the default values that correspond to the given classes.
+ *
  * @author Alexander Shabanov
  */
 public final class DefaultValues {
@@ -10,9 +12,21 @@ public final class DefaultValues {
     if (clazz.isPrimitive()) {
       if (int.class.equals(clazz)) {
         return 0;
+      } else if (byte.class.equals(clazz)) {
+        return (byte) 0;
+      } else if (char.class.equals(clazz)) {
+        return (char) 0;
+      } else if (short.class.equals(clazz)) {
+        return (short) 0;
+      } else if (long.class.equals(clazz)) {
+        return (long) 0;
+      } else if (float.class.equals(clazz)) {
+        return 0.0f;
+      } else if (double.class.equals(clazz)) {
+        return 0.0d;
       }
 
-      throw new UnsupportedOperationException();
+      throw new UnsupportedOperationException("Unknown primitive type: " + clazz);
     }
 
     return null;
