d223434deb8854887de98a5017521d0885fd51ea
Alexander Shabanov <avshabanov@gmail.com>
2013-12-31 19:57:08 -0800

    Adding JcoFactory

--- polymer-code/src/main/java/com/truward/polymer/code/JcoVisitor.java
+++ polymer-code/src/main/java/com/truward/polymer/code/JcoVisitor.java
@@ -10,7 +10,25 @@ public abstract class JcoVisitor {
     throw new IllegalStateException("Unhandled node " + node);
   }
 
-  //public void visitClassDecl(@N)
+  public void visitExpr(@Nonnull Jco.Expr node) {
+    visitNode(node);
+  }
 
+  public void visitCharExpr(@Nonnull Jco.CharExpr node) {
+    visitExpr(node);
+  }
 
+  public void visitText(@Nonnull Jco.Text node) {
+    visitExpr(node);
+  }
+
+  public static void apply(@Nonnull JcoVisitor visitor, @Nonnull Jco.Node node) {
+    if (node instanceof Jco.CharExpr) {
+      visitor.visitCharExpr((Jco.CharExpr) node);
+    } else if (node instanceof Jco.Text) {
+      visitor.visitText((Jco.Text) node);
+    } else {
+      visitor.visitNode(node);
+    }
+  }
 }
